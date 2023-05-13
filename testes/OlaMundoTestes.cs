20afc75a6cfaa6650efa7d102701c8984fae5cbe
Steve Silva <Steve.Silva@copart.com>
2023-05-12 21:01:04 -0300

    add testes/OlaMundoTestes

---
+++ testes/OlaMundoTestes.cs
@@ -0,0 +1,14 @@
+using System;
+using CSharp;
+using ex1;
+
+namespace Testes
+{
+    public class OlaMundoTestes
+    {
+        public static void Execute()
+        {
+            OlaMundo.Execute();
+        }
+    }
+}
